e4fad91f1afd1158944592fe5cf289f2a955a437
Practical-UVM-Step-By-Step <srivatsa9@gmail.com>
2019-07-01 10:47:07 -0700

    Delete .synopsys_dve_rebuild.tcl

--- soc/tb/uvm/wb_conmax/.synopsys_dve_rebuild.tcl
+++
@@ -1,8 +0,0 @@
-# DVE Simulation Rebuild/Restart Options
-# Saved on Thu Feb 21 22:43:46 2013
-set SIMSETUP::REBUILDOPTION 0
-set SIMSETUP::REBUILDCMD {./comp}
-set SIMSETUP::REBUILDDIR {./simv  +UVM_NO_RELNOTES  -l run.log}
-set SIMSETUP::RESTOREBP 1
-set SIMSETUP::RESTOREDUMP 1
-set SIMSETUP::RESTOREFORCE 1
